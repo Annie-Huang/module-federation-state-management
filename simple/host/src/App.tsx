cd42ffa6069569c69286b4d05b36fb57c742f910
Annie Huang <annie.hn@gmail.com>
2024-05-19 13:10:35 +1000

    Add counter state into <App> in host app

--- simple/host/src/App.tsx
+++ simple/host/src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Header from 'nav/Header';
 
 import './index.scss';
 
-const App = () => (
-  <div className='text-3xl mx-auto max-w-6xl'>
-    <Header />
-    <div>Name: host</div>
-  </div>
-);
+const App = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className='text-3xl mx-auto max-w-6xl'>
+      <Header />
+      <div>Name: host</div>
+      <div>Count: {count}</div>
+      <div>Name: host</div>
+      <div>
+        <button
+          onClick={() => setCount(count + 1)}
+          className='bg-indigo-800 text-white font-bold py-2 px-4 rounded'
+        >
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
+};
 const rootElement = document.getElementById('app');
 if (!rootElement) throw new Error('Failed to find the root element');
 
