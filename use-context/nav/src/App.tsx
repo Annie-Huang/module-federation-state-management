1942bae3b4ca149a8a0f6af9d3b7da8a11ba951c
Annie Huang <annie.hn@gmail.com>
2024-05-19 17:41:39 +1000

    Fixed error on <App> in nav app in both folders

--- use-context/nav/src/App.tsx
+++ use-context/nav/src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Header from './Header';
 
 import './index.scss';
 
-const App = () => (
-  <div className='mt-10 text-3xl mx-auto max-w-6xl'>
-    <Header />
-    <div>Name: nav</div>
-  </div>
-);
+const App = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className='mt-10 text-3xl mx-auto max-w-6xl'>
+      <Header count={count} onClear={() => setCount(0)} />
+      <div>Name: nav</div>
+    </div>
+  );
+};
 const rootElement = document.getElementById('app');
 if (!rootElement) throw new Error('Failed to find the root element');
 
