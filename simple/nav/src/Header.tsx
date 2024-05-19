a39ab09291956e1c1a81685da98a8fc82c16e5fc
Annie Huang <annie.hn@gmail.com>
2024-05-19 12:54:01 +1000

    Create <Header> and use in <App> in nav app

---
+++ simple/nav/src/Header.tsx
@@ -0,0 +1,18 @@
+// import React, { FC } from 'react';
+import React from 'react';
+
+// interface HeaderProps {
+//   count: number;
+//   onClear: () => void;
+// }
+
+// const Header: FC<HeaderProps> = ({ count, onClear }) => {
+const Header = () => {
+  return (
+    <header className='bg-blue-700 text-white font-bold text-3xl p-5 flex'>
+      <div>Header</div>
+    </header>
+  );
+};
+
+export default Header;
