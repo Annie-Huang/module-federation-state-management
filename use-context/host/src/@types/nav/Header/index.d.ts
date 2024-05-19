0b0344e486e3fcc31bfe5fa6ece9745e7a196c3e
Annie Huang <annie.hn@gmail.com>
2024-05-19 22:13:53 +1000

    Couldn't get @types/host/store/index.d.ts working.

--- use-context/host/src/@types/nav/Header/index.d.ts
+++ use-context/host/src/@types/nav/Header/index.d.ts
@@ -1,3 +1,4 @@
+// This is not quite right, as the new <Header> component does not need to pass in any props.
 import React from 'react';
 const HeaderProps: React.FunctionComponent<{
   count: number;
