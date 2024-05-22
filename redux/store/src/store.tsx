f93d986b496434a57f00dfe4fa5d87cfac0b3102
Annie Huang <annie.hn@gmail.com>
2024-05-22 22:02:16 +1000

    Create the store.tsx and copy the store boilerplate and state-slice boilerplate from redux-toolkit doc into here.

---
+++ redux/store/src/store.tsx
@@ -0,0 +1,49 @@
+// https://redux-toolkit.js.org/tutorials/quick-start#create-a-redux-store
+// https://redux-toolkit.js.org/tutorials/quick-start#create-a-redux-state-slice
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+export interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
+
+export const store = configureStore({
+  reducer: {},
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
