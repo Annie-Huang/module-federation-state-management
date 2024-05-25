ac17a18380e9038af3ca68337599c1be4178a7e8
Annie Huang <annie.hn@gmail.com>
2024-05-25 12:14:31 +1000

    Copy <App> in nav from use-context to redux as well and made adjustment.

--- redux/nav/src/App.tsx
+++ redux/nav/src/App.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import './index.scss'
+import Header from './Header';
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: nav</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+import './index.scss';
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
+import { StoreProvider } from 'store/store';
 
-root.render(<App />)
+const App = () => {
+  return (
+    <StoreProvider>
+      <div className='mt-10 text-3xl mx-auto max-w-6xl'>
+        <Header />
+        <div>Name: nav</div>
+      </div>
+    </StoreProvider>
+  );
+};
+const rootElement = document.getElementById('app');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(<App />);
